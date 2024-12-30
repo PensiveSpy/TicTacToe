6f8fd6ea72475b49d97fcf2a571bae1be8f44972
Jason <139952340+PensiveSpy@users.noreply.github.com>
2024-12-30 03:32:17 -0700

    Edited the ActionListener to stop player overriding

--- TicTacToe.java
+++ TicTacToe.java
@@ -54,10 +54,13 @@ public class TicTacToe {
                 tile.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e){
                         JButton tile = (JButton) e.getSource();
-                        tile.setText(currentPlayer);
+                        if (tile.getText() == ""){
+                            tile.setText(currentPlayer);
 
-                        currentPlayer = currentPlayer == playerX ? playerO : (currentPlayer == playerO ? playerY : playerX);
-                        textLabel.setText(currentPlayer + " 's turn.");
+                            currentPlayer = currentPlayer == playerX ? playerO : (currentPlayer == playerO ? playerY : playerX);
+                            textLabel.setText(currentPlayer + " 's turn.");
+                        }
+
                     }
                 });
             }
