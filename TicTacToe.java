5df4714d660dc3ba401c8b2b9766e9359f7347b6
Jason <139952340+PensiveSpy@users.noreply.github.com>
2024-12-30 02:39:54 -0700

    Editing the JavaFX textLabel

--- TicTacToe.java
+++ TicTacToe.java
@@ -16,7 +16,15 @@ public class TicTacToe {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        textLabel.setBackground(Color.red);
+        textLabel.setBackground(new Color(137, 171, 227));
+        textLabel.setForeground(new Color(252, 246, 245));
+        textLabel.setFont(new Font("Times New Roman", Font.BOLD, 60 ));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe Game");
+        textLabel.setOpaque(true);
+
+
+
 
     }
 }
