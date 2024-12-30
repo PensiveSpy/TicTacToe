ca649cd21a51b7593ffc4a2087b4bbbdbe7a5bd3
Jason <139952340+PensiveSpy@users.noreply.github.com>
2024-12-29 19:48:22 -0700

    Added the App and TicTacToe classes

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,2 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class TicTacToe {
+    int boardWidth = 600;
+    int boardHeight = 650;
+
+    JFrame frame = new JFrame();
+    TicTacToe(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+    }
 }
