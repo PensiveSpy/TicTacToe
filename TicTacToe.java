6d1fd7e546fa950398c16262dd394c0394627f9f
Jason <139952340+PensiveSpy@users.noreply.github.com>
2024-12-30 02:27:59 -0700

    Adding the Java Frame

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,14 +1,22 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 public class TicTacToe {
-    int boardWidth = 600;
-    int boardHeight = 650;
+    int boardWidth = 1920;
+    int boardHeight = 1080;
 
     JFrame frame = new JFrame();
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
     TicTacToe(){
         frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.red);
+
     }
 }
